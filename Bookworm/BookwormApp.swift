27607b138aa9a38d7ee3bc5f7a4a657916640ad9
mlwelker <nutria.watts.0g@icloud.com>
2023-08-26 13:25:15 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Michael Welker on 2023-08-26.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
